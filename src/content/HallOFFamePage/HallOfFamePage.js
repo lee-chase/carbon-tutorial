694cabb449ef8ddfce0413e5e4fbc2f1544dda7f
Lee Chase <lee.chase@uk.ibm.com>
2019-08-19 22:48:28 +0100

    chore: in progress

--- src/content/HallOFFamePage/HallOfFamePage.js
+++ src/content/HallOFFamePage/HallOfFamePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import HallOfFameList from './HallOfFameList';
 import { Tile } from 'carbon-components-react';
+import { TextInput } from 'carbon-components-react';
 
 const HallOfFamePage = () => {
   const [filter, setFilter] = useState(
@@ -8,7 +9,7 @@ const HallOfFamePage = () => {
   );
 
   return (
-    <div className="bx--grid bx--grid--full-width">
+    <div className="bx--grid bx--grid--full-width bx--grid--no-gutter">
       <div className="hof-page__banner">
         <div className="bx--row">
           <div className="bx--col-lg-16">
@@ -18,7 +19,7 @@ const HallOfFamePage = () => {
           </div>
         </div>
         <div className="bx--row">
-          <Tile className="bx--offset-lg-4 bx--col-lg-4">
+          <Tile className="bx--offset-lg-4 bx--col-lg-4 hof-page__banner-tile">
             <p class="hof-page__banner-text">These folks got one of these...</p>
             <img
               className="hof-page__image"
@@ -26,7 +27,7 @@ const HallOfFamePage = () => {
               alt="Carbon developer essentials react badge"
             />
           </Tile>
-          <Tile className="bx--col-lg-4">
+          <Tile className="bx--col-lg-4 hof-page__banner-tile">
             <img
               className="hof-page__image"
               src={`${process.env.PUBLIC_URL}/carbon-tee.png`}
@@ -39,16 +40,13 @@ const HallOfFamePage = () => {
         </div>
         <div className="bx--row">
           <div className="bx--offset-lg-4 bx--col-lg-8">
-            <label>
-              Filter:
-              <input
-                type="text"
-                value={filter}
-                onChange={e => {
-                  setFilter(e.target.value);
-                }}
-              />
-            </label>
+            <TextInput
+              labelText="Filter"
+              value={filter}
+              onChange={e => {
+                setFilter(e.target.value);
+              }}
+            />
           </div>
         </div>
       </div>
